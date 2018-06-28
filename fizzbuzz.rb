1a7cd37963ee15533845f4094c381eca02d4216a
Sav Younts <savyounts@gmail.com>
2018-06-28 02:21:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz (int)
   if int % 3 === 0 
     "Fizz"
-  elsif int % 5 === 0 
+  if int % 5 === 0 
     "Buzz"
-  elsif (int % 3 && 5) === 0  
+  if (int % 3 && 5) === 0  
     "Fizzbuzz"
   end  
 end  
